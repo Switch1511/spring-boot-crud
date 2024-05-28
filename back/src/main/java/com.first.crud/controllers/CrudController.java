98be1f9825be864d77495031e5543a339d82201e
Switch1511 <gustavopasqualli0@gmail.com>
2024-05-28 19:27:26 -0300

    Created method post and get in controller

--- back/src/main/java/com.first.crud/controllers/CrudController.java
+++ back/src/main/java/com.first.crud/controllers/CrudController.java
@@ -1,8 +1,25 @@
 package com.first.crud.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 
+@RequestMapping("/")
 public class CrudController {
+
+    private List<String> items = new ArrayList<>();
+
+    @GetMapping
+    public List<String> getItems() {
+        return items;
+    }
+
+    @PostMapping
+    public String addItem(@RequestBody String item) {
+        items.add(item);
+        return "Item added successfully";
+    }
 }
