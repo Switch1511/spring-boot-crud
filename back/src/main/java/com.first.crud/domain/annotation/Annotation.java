3207d6450883f7051249a140ad9e2369950df966
Switch1511 <gustavopasqualli0@gmail.com>
2024-05-30 22:12:51 -0300

    Add annotation collumn in annotation domain

--- back/src/main/java/com.first.crud/domain/annotation/Annotation.java
+++ back/src/main/java/com.first.crud/domain/annotation/Annotation.java
@@ -1,14 +1,20 @@
 package com.first.crud.domain.annotation;
 
-import lombok.EqualsAndHashCode;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Table(name = "annotation")
 @Entity(name = "annotation")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Annotation {
 
     @Id @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private String id;
+
+    private String annotation;
 }
